dde21bc1545da4cb729a9297588e9a3eae1c3f6d
thanawatgulati <thanawatgulatijames@gmail.com>
2020-02-01 12:11:11 +0700

    🔥 edit gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thanawat Gulati`,
+    description: `Thanawat Gulati Blog`,
+    author: `@thanawatgulati`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
