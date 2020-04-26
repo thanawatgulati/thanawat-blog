0557d4e84ff86854fcf6e22873e1b23c2b533340
thanawatgulati <thanawatgulatijames@gmail.com>
2020-04-26 17:19:51 +0700

    🍺 Setup Contentful in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,14 @@
+const dotenv = require("dotenv")
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thanawat Gulati`,
+    description: `A weblog that tells about work experience. Product reviews, both technology and many other stories.`,
+    author: `@thanawatgulati`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +24,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `M'James Thanawat Blog`,
+        short_name: `M'James Blog`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f6a6b9`,
+        theme_color: `#f6a6b9`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qr4fdn101ci2`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
