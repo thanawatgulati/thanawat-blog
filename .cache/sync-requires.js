7358595fab7103a7f101cb0d4b29d9ef85a822a7
thanawatgulati <thanawatgulatijames@gmail.com>
2020-05-20 17:30:14 +0700

    📦 Update Pakage.json

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/thanawatgulati/Desktop/gatsby/new-blog-2020/.cache/dev-404-page.js")))
+}
+
