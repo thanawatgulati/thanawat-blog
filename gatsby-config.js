1ac99d47d5758d23fb01c2a2c88e79f18649e43a
thanawatgulati <thanawatgulatijames@gmail.com>
2020-05-21 13:31:40 +0700

    ☕️ Setup gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  siteMetadata: {
+    title: `Thanawat Gulati`,
+    description: `Thanawat Gulati - Blog , Work experience portfolio and more`,
+    author: `@thanawatgulati`,
+  },
+  plugins: [],
+};
