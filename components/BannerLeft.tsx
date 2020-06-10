7433576f8710749754122064db8c53ab3ea3bc0e
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-11 02:59:47 +0700

    🌆 LazyLoad image

--- components/BannerLeft.tsx
+++ components/BannerLeft.tsx
@@ -1,7 +1,17 @@
 import React, { ReactElement } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import styles from "../styles/bannerLeft.module.css";
 interface Props {}
 
 export default function BannerLeft({}: Props): ReactElement {
-  return <div className={styles.bannerLeft}></div>;
+  return (
+    <div className={styles.bannerLeft}>
+      <LazyLoadImage
+        className={styles.imgBannerLeft}
+        src="/images/bannerLeft.jpg"
+        alt="banner-left"
+        effect="blur"
+      />
+    </div>
+  );
 }
