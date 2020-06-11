b5f02f300824a364cc64f1be3bcaa8cf44a21681
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-11 20:41:30 +0700

    🌃 Tach To Night

--- pages/ttn.tsx
+++ pages/ttn.tsx
@@ -3,5 +3,5 @@ import React, { ReactElement } from "react";
 interface Props {}
 
 export default function TechToNight({}: Props): ReactElement {
-  return <div></div>;
+  return <div>Tech To Knight</div>;
 }
