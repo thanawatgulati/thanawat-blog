e3d1fd9baa4824e5457222793932fca5b12d2d82
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-11 22:14:01 +0700

    😢 Reset NavigationBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,24 +5,5 @@ import Link from "next/link";
 interface Props {}
 
 export default function Navbar({}: Props): ReactElement {
-  const [action, setaction] = useState(false);
-  return (
-    <nav className={styles.nav}>
-      <Link href="/">
-        <label className={styles.logo}>G.THANAWAT</label>
-      </Link>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <Link href="/">
-            <a className={styles.a}>Home</a>
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/me">
-            <a className={styles.a}>About:me</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  return <div></div>;
 }
