1510eac66ae8a4e8fee56db65909ef3988db2eb1
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-11 20:32:14 +0700

    📱 Responsive NavigationBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,30 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import styles from "../styles/navbar.module.css";
 import Link from "next/link";
 
 interface Props {}
 
 export default function Navbar({}: Props): ReactElement {
+  const [action, setaction] = useState(false);
   return (
     <nav className={styles.nav}>
       <input type="checkbox" id={styles.check} />
-      <label className={styles.checkbtn}>🍔</label>
+      <label className={styles.checkbtn}>
+        <button
+          className={styles.hamburg}
+          onClick={() => {
+            if (!action) {
+              document.getElementById(styles.check).checked = true;
+              setaction(true);
+            } else {
+              document.getElementById(styles.check).checked = false;
+              setaction(false);
+            }
+          }}
+        >
+          <h4>🍔</h4>
+        </button>
+      </label>
       <Link href="/">
         <label className={styles.logo}>G.THANAWAT</label>
       </Link>
